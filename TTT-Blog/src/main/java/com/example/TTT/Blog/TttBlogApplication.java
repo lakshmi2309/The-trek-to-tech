72d2799dd876f9c61856127f78d9f13be8ac3520
Lakshmi Linga <lakshmi.linga@version1.com>
2022-06-17 14:42:00 +0100

    Initial commit

---
+++ TTT-Blog/src/main/java/com/example/TTT/Blog/TttBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.TTT.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttBlogApplication.class, args);
+	}
+
+}
---
+++ TTT-Blog/src/test/java/com/example/TTT/Blog/TttBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TTT.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
